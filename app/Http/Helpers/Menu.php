f42730e78d992f4e02d405d5accce70c0a9e5ccd
Alan Johnson <aj@alanjohnson.ie>
2018-01-30 10:00:24 +0000

    re-arrange menu

--- app/Http/Helpers/Menu.php
+++ app/Http/Helpers/Menu.php
@@ -5,30 +5,43 @@ class Menu {
 	public static function activeMenu($uri='')
 	{
 		$active = '';
+		if(is_array($uri)){
+			$active = self::activeMenuArr($uri);
+		} else {
+			$active = self::activeMenuString($uri);
+		}
+		return $active;
+	}
 
-		//dd(\Request::route()->getName());
+	private static function activeMenuString($uri='')
+	{
+		$active = '';
 		$arr = explode('.', $uri);
-		//dd(Request::segment(1));
 
 		if($uri === 'home' &&  Request::segment(1) === null){
 			$active = 'active';			
 		}
 
 		if(count($arr) === 2){
-			if ( Request::route()->getName() === $uri || (Request::segment(1) === $arr[0] && Request::segment(1) === $arr[1] ) )
-			{
+			if ( Request::route()->getName() === $uri || (Request::segment(1) === $arr[0] && Request::segment(1) === $arr[1] ) ){
 				$active = 'active';
 			}
 		}
 		else{
 			//dd(Request::segment(1));
-			if ( (Request::segment(1) === $arr[0] ) )
-			{
+			if ( (Request::segment(1) === $arr[0] ) ){
 				$active = 'active';
 			}
 		}
+		return $active;		
+	}
 
-
+	private static function activeMenuArr($uri = [])
+	{
+		$active = '';
+		foreach($uri as $url){
+			$active .= self::activeMenuString($url);
+		}
 		return $active;
 	}
 	
--- resources/views/layouts/partials/sidebar.blade.php
+++ resources/views/layouts/partials/sidebar.blade.php
@@ -31,76 +31,6 @@
         </ul>
       </li>
 
-      <li class="treeview {{ Menu::activeMenu('jobs') }}">
-        <a href="#">
-          <i class="fa fa-briefcase"></i>
-          <span>Jobs</span>
-          <span class="pull-right-container">
-            <i class="fa fa-angle-left pull-right"></i>
-          </span>
-        </a>
-        <ul class="treeview-menu">
-          <li class="{{ Menu::activeMenu('jobs.index') }}"><a href="{{ route('jobs.index') }}"><i class="fa fa-list"></i> All Jobs</a></li>
-            @can('create-job')
-            <li class="{{ Menu::activeMenu('jobs.create') }}">
-              <a href="{{ route('jobs.create') }}"><i class="fa fa-plus"></i>Add Job</a>
-            </li>
-            @endcan
-        </ul>
-      </li>
-
-      <li class="treeview {{ Menu::activeMenu('clients') }}">
-        <a href="#">
-          <i class="fa fa-vcard"></i>
-          <span>Clients</span>
-          <span class="pull-right-container">
-            <i class="fa fa-angle-left pull-right"></i>
-          </span>
-        </a>
-        <ul class="treeview-menu">
-          <li class="{{ Menu::activeMenu('clients.index') }}"><a href="{{ route('clients.index') }}"><i class="fa fa-list"></i> All Clients</a></li>
-          @can('create-client')
-          <li class="{{ Menu::activeMenu('clients.create') }}">
-            <a href="{{ route('clients.create') }}"><i class="fa fa-plus"></i>Add Client</a>
-          </li>
-          @endcan
-        </ul>
-      </li>
-
-      <li class="treeview {{ Menu::activeMenu('users') }}">
-        <a href="#">
-          <i class="fa fa-user"></i>
-          <span>Users</span>
-          <span class="pull-right-container">
-            <i class="fa fa-angle-left pull-right"></i>
-          </span>
-        </a>
-        <ul class="treeview-menu">
-          <li class="{{ Menu::activeMenu('users.index') }}"><a href="{{ route('users.index') }}"><i class="fa fa-list"></i> All Users</a></li>
-            @can('create-user')
-            <li class="{{ Menu::activeMenu('users.create') }}">
-              <a href="{{ url('/register') }}"><i class="fa fa-plus"></i>Add User</a>
-            </li>
-            @endcan
-        </ul>
-      </li>
-
-      <li class="treeview {{ Menu::activeMenu('quotes') }}">
-        <a href="#">
-          <i class="fa fa-euro"></i>
-          <span>Quotes</span>
-          <span class="pull-right-container">
-            <i class="fa fa-angle-left pull-right"></i>
-          </span>
-        </a>
-        <ul class="treeview-menu">
-          <li class="{{ Menu::activeMenu('quotes.index') }}"><a href="{{ route('quotes.index') }}"><i class="fa fa-list"></i> All Quotes</a></li>
-          @can('create-quote')
-            <li class="{{ Menu::activeMenu('quotes.create') }}"><a href="{{ route('quotes.create') }}"><i class="fa fa-plus"></i>Add Quote</a></li> 
-          @endcan
-        </ul>
-      </li>
-
       <li class="treeview {{ Menu::activeMenu('reports.index') }}">
         <a href="#">
           <i class="fa fa-bar-chart"></i>
@@ -114,21 +44,113 @@
         </ul>
       </li>
 
-      <li class="treeview {{ Menu::activeMenu('tasktypes') }}">
+      
+      <li class="treeview {{ Menu::activeMenu(['users','clients', 'jobs', 'quotes', 'tasktypes']) }}">
         <a href="#">
-          <i class="fa fa-tags"></i>
-          <span>Task Types</span>
+          <i class="fa fa-lock"></i> <span>Admin</span>
           <span class="pull-right-container">
             <i class="fa fa-angle-left pull-right"></i>
           </span>
         </a>
         <ul class="treeview-menu">
-          <li class="{{ Menu::activeMenu('tasktypes.index') }}"><a href="{{ route('tasktypes.index') }}"><i class="fa fa-list"></i> All Task Types</a></li>
-            @can('create-job')
-            <li class="{{ Menu::activeMenu('tasktypes.create') }}">
-              <a href="{{ route('tasktypes.create') }}"><i class="fa fa-plus"></i>Add Tasktype</a>
+
+            {{-- Users --}}
+            <li class="treeview {{ Menu::activeMenu('users') }}">
+              <a href="#">
+                <i class="fa fa-user"></i>
+                <span>Users</span>
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li class="{{ Menu::activeMenu('users.index') }}"><a href="{{ route('users.index') }}"><i class="fa fa-list"></i> All Users</a></li>
+                  @can('create-user')
+                  <li class="{{ Menu::activeMenu('users.create') }}">
+                    <a href="{{ url('/register') }}"><i class="fa fa-plus"></i>Add User</a>
+                  </li>
+                  @endcan
+              </ul>
             </li>
-            @endcan
+            {{-- End Users --}}
+
+            {{-- Clients --}}
+            <li class="treeview {{ Menu::activeMenu('clients') }}">
+              <a href="#">
+                <i class="fa fa-vcard"></i>
+                <span>Clients</span>
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li class="{{ Menu::activeMenu('clients.index') }}"><a href="{{ route('clients.index') }}"><i class="fa fa-list"></i> All Clients</a></li>
+                @can('create-client')
+                <li class="{{ Menu::activeMenu('clients.create') }}">
+                  <a href="{{ route('clients.create') }}"><i class="fa fa-plus"></i>Add Client</a>
+                </li>
+                @endcan
+              </ul>
+            </li>
+            {{-- End Clients --}}
+
+            {{-- Jobs --}}
+            <li class="treeview {{ Menu::activeMenu('jobs') }}">
+              <a href="#">
+                <i class="fa fa-briefcase"></i>
+                <span>Jobs</span>
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li class="{{ Menu::activeMenu('jobs.index') }}"><a href="{{ route('jobs.index') }}"><i class="fa fa-list"></i> All Jobs</a></li>
+                  @can('create-job')
+                  <li class="{{ Menu::activeMenu('jobs.create') }}">
+                    <a href="{{ route('jobs.create') }}"><i class="fa fa-plus"></i>Add Job</a>
+                  </li>
+                  @endcan
+              </ul>
+            </li>
+            {{-- End Jobs --}}
+
+            {{-- Quotes --}}
+            <li class="treeview {{ Menu::activeMenu('quotes') }}">
+              <a href="#">
+                <i class="fa fa-euro"></i>
+                <span>Quotes</span>
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li class="{{ Menu::activeMenu('quotes.index') }}"><a href="{{ route('quotes.index') }}"><i class="fa fa-list"></i> All Quotes</a></li>
+                @can('create-quote')
+                  <li class="{{ Menu::activeMenu('quotes.create') }}"><a href="{{ route('quotes.create') }}"><i class="fa fa-plus"></i>Add Quote</a></li> 
+                @endcan
+              </ul>
+            </li>
+            {{-- End Quotes --}}
+
+            {{-- Tasktypes --}}
+            <li class="treeview {{ Menu::activeMenu('tasktypes') }}">
+              <a href="#">
+                <i class="fa fa-tags"></i>
+                <span>Task Types</span>
+                <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                </span>
+              </a>
+              <ul class="treeview-menu">
+                <li class="{{ Menu::activeMenu('tasktypes.index') }}"><a href="{{ route('tasktypes.index') }}"><i class="fa fa-list"></i> All Task Types</a></li>
+                  @can('create-job')
+                  <li class="{{ Menu::activeMenu('tasktypes.create') }}">
+                    <a href="{{ route('tasktypes.create') }}"><i class="fa fa-plus"></i>Add Tasktype</a>
+                  </li>
+                  @endcan
+              </ul>
+            </li>
+            {{-- End Tasktypes --}}
         </ul>
       </li>
 
